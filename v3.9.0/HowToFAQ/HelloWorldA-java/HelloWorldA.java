ec22496f1cb7427bf72d69faf9c890a3f74c3ff1
davidcok <davidcok@github.com>
2022-10-04 17:13:42 -0400

    Documentation snapshot for v3.9.0

---
+++ v3.9.0/HowToFAQ/HelloWorldA-java/HelloWorldA.java
@@ -0,0 +1,13 @@
+// Dafny program HelloWorldA.dfy compiled into Java
+
+import _System.*;
+
+
+
+
+
+public class HelloWorldA {
+  public static void main(String[] args) {
+    dafny.Helpers.withHaltHandling(_System.__default::__Main);
+  }
+}
